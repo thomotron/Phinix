52d934f9c1f3842f3b2da8ba6b19314689c728c4
Thomotron <gigatech55@gmail.com>
2018-08-09 21:52:46 +0930

    Add basic server connections framework and tests

---
+++ Common/Connections/Server.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using NetworkCommsDotNet;
+using NetworkCommsDotNet.Connections;
+using NetworkCommsDotNet.Connections.TCP;
+using ProtoBuf;
+
+namespace Connections
+{
+    public static class Server
+    {
+        public static bool Listening => Connection.Listening(ConnectionType.TCP);
+
+        /// <summary>
+        /// Registers event handlers for NetworkComms.Net for connection established, connection closed, and incoming data.
+        /// </summary>
+        public static void RegisterHandlers()
+        {
+            NetworkComms.AppendGlobalConnectionEstablishHandler(ConnectionEstablishedHandler);
+            NetworkComms.AppendGlobalConnectionCloseHandler(ConnectionClosedHandler);
+            NetworkComms.AppendGlobalIncomingPacketHandler<byte[]>("Phinix", IncomingPacketHandler);
+        }
+
+        /// <summary>
+        /// Unregisters event handlers for NetworkComms.Net. Effectively the opposite of <c>RegisterHandlers()</c>.
+        /// </summary>
+        public static void UnregisterHandlers()
+        {
+            NetworkComms.RemoveGlobalConnectionEstablishHandler(ConnectionEstablishedHandler);
+            NetworkComms.RemoveGlobalConnectionCloseHandler(ConnectionClosedHandler);
+            NetworkComms.RemoveGlobalIncomingPacketHandler("Phinix");
+        }
+
+        /// <summary>
+        /// Starts listening for connections on the given endpoint.
+        /// </summary>
+        /// <param name="endpoint">Endpoint to bind to</param>
+        public static void Start(IPEndPoint endpoint)
+        {
+            if (!Listening)
+            {
+                Connection.StartListening(ConnectionType.TCP, endpoint);
+            }
+            else
+            {
+                throw new Exception("Cannot start listening while already doing so.");
+            }
+        }
+
+        /// <summary>
+        /// Stops listening for new connections and terminates any active ones.
+        /// </summary>
+        public static void Stop()
+        {
+            Connection.StopListening();
+            NetworkComms.CloseAllConnections(ConnectionType.TCP);
+        }
+
+        /// <summary>
+        /// Placeholder for processing opened connections.
+        /// </summary>
+        /// <param name="connection"></param>
+        public static void ConnectionEstablishedHandler(Connection connection)
+        {
+
+        }
+
+        /// <summary>
+        /// Placeholder for processing closed connecctions.
+        /// </summary>
+        /// <param name="connection"></param>
+        public static void ConnectionClosedHandler(Connection connection)
+        {
+
+        }
+
+        /// <summary>
+        /// Placeholder for processing incoming packets.
+        /// </summary>
+        /// <param name="header"></param>
+        /// <param name="connection"></param>
+        /// <param name="incomingObject"></param>
+        public static void IncomingPacketHandler(PacketHeader header, Connection connection, byte[] incomingObject)
+        {
+
+        }
+    }
+}
---
+++ Common/ConnectionsTests/ServerTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Net;
+using NetworkCommsDotNet;
+using NetworkCommsDotNet.Connections;
+using NUnit.Framework;
+
+namespace Connections.Tests
+{
+    [TestFixture]
+    public class ServerTests
+    {
+        [Test]
+        public void RegisterHandlersTest()
+        {
+            // It doesn't seem possible to verify whether the connection established and closing handlers have been registered or not so this'll have to do.
+            // This test is pretty much 1/3rd pointless because of that.
+            NetworkComms.RemoveGlobalIncomingPacketHandler("Phinix");
+
+            Assert.IsFalse(NetworkComms.GlobalIncomingPacketHandlerExists("Phinix"));
+
+            Server.RegisterHandlers();
+
+            Assert.IsTrue(NetworkComms.GlobalIncomingPacketHandlerExists("Phinix"));
+
+            NetworkComms.RemoveGlobalIncomingPacketHandler("Phinix");
+        }
+
+        [Test]
+        public void UnregisterHandlers()
+        {
+            NetworkComms.AppendGlobalIncomingPacketHandler<byte[]>("Phinix", (header, connection, incomingObject) => { });
+
+            Assert.IsTrue(NetworkComms.GlobalIncomingPacketHandlerExists("Phinix"));
+
+            Server.UnregisterHandlers();
+
+            Assert.IsFalse(NetworkComms.GlobalIncomingPacketHandlerExists("Phinix"));
+
+            NetworkComms.RemoveGlobalIncomingPacketHandler("Phinix");
+        }
+        
+        [Test]
+        public void ListeningTest()
+        {
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Any, 0);
+
+            Connection.StartListening(ConnectionType.TCP, endpoint);
+            
+            Assert.AreEqual(Connection.Listening(ConnectionType.TCP), Server.Listening);
+            Assert.IsTrue(Server.Listening);
+
+            Connection.StopListening(ConnectionType.TCP);
+            
+            Assert.AreEqual(Connection.Listening(ConnectionType.TCP), Server.Listening);
+            Assert.IsFalse(Server.Listening);
+        }
+
+        [Test]
+        public void StartTest()
+        {
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Any, 0);
+
+            Assert.IsFalse(Connection.Listening(ConnectionType.TCP));
+
+            Server.Start(endpoint);
+
+            Assert.IsTrue(Connection.Listening(ConnectionType.TCP));
+
+            Assert.Catch(() =>
+            {
+                Server.Start(endpoint);
+            });
+
+            Connection.StopListening(ConnectionType.TCP);
+
+            Assert.IsFalse(Connection.Listening(ConnectionType.TCP));
+        }
+
+        [Test]
+        public void StopTest()
+        {
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Any, 0);
+            Connection.StartListening(ConnectionType.TCP, endpoint);
+
+            Assert.IsTrue(Connection.Listening(ConnectionType.TCP));
+
+            Server.Stop();
+
+            Assert.IsFalse(Connection.Listening(ConnectionType.TCP));
+        }
+    }
+}
