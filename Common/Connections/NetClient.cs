62068dff28dd2572eb2f908802096ec58c4a0b41
Thomotron <gigatech55@gmail.com>
2018-08-13 01:05:36 +0930

    WIP: IPs, Hostnames, Failed Connections, Oh My!

--- Common/Connections/NetClient.cs
+++ Common/Connections/NetClient.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using NetworkCommsDotNet;
 using NetworkCommsDotNet.Connections.TCP;
 using ProtoBuf;
@@ -17,13 +20,44 @@ namespace Connections
         /// Attempts to connect to the given endpoint. This will close an existing connection.
         /// </summary>
         /// <param name="endpoint">Endpoint to connect to</param>
-        public void TryConnect(IPEndPoint endpoint)
+        public void Connect(IPEndPoint endpoint)
         {
             // Close the active connection before we make a new one.
             Disconnect();
 
             ConnectionInfo connectionInfo = new ConnectionInfo(endpoint);
             connection = TCPConnection.GetConnection(connectionInfo);
+        }
+
+        /// <summary>
+        /// Attempts to parse the given 
+        /// </summary>
+        /// <param name="hostname"></param>
+        /// <param name="port"></param>
+        /// <returns></returns>
+        public bool TryConnect(string hostname, int port)
+        {
+            IPAddress address;
+            if (!IPAddress.TryParse(hostname, out address))
+            {
+                if (!TryQueryHostname(hostname, out address))
+                {
+                    return false;
+                }
+            }
+            
+            Connect(new IPEndPoint(address, port));
+            return true;
+        }
+
+        private bool TryQueryHostname(string hostname, out IPAddress address)
+        {
+            IPAddress[] addresses = Dns.GetHostAddresses(hostname);
+
+            // TODO: Get some creamy IPv6 support up in here
+            address = addresses.First(ip => ip.AddressFamily == AddressFamily.InterNetwork);
+
+            return address != null;
         }
 
         /// <summary>
