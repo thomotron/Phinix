5a29ca048d710a139edf162d0b9bb1b831ea1f27
Thomotron <gigatech55@gmail.com>
2018-08-18 14:18:34 +0930

    Add version command

---
+++ Server/Commands/VersionCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace PhinixServer
+{
+    public class VersionCommand : Command
+    {
+        public override string CommandName => "version";
+
+        public override HelpEntry[] HelpEntries => new HelpEntry[]
+        {
+            new HelpEntry("version", "Displays the version of the server and its modules")
+        };
+
+        public override bool Execute(List<string> args)
+        {
+            Console.WriteLine("Server: " + Assembly.GetExecutingAssembly().GetName().Version);
+            Console.WriteLine("Connections: " + Assembly.GetAssembly(typeof(Connections.NetServer)).GetName().Version);
+
+            return true;
+        }
+    }
+}
