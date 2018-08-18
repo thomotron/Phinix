6d7537b316f8efda07d0fdc7391c97a906c63621
Thomotron <gigatech55@gmail.com>
2018-08-19 01:55:19 +0930

    Add documentation for Logger

--- Server/Logger.cs
+++ Server/Logger.cs
@@ -5,18 +5,37 @@ using System.Text;
 
 namespace PhinixServer
 {
+    /// <summary>
+    /// Appends messages with timestamps and severity levels to the console and disk.
+    /// </summary>
     public class Logger
     {
+        /// <summary>
+        /// The minimum severity level for a message to be displayed in the console.
+        /// </summary>
         public Severity MinimumDisplaySeverity;
 
+        /// <summary>
+        /// Path to the log file.
+        /// </summary>
         private string logPath;
 
+        /// <summary>
+        /// Creates a new <c>Logger</c> instance.
+        /// </summary>
+        /// <param name="logPath">Log file path including extension</param>
+        /// <param name="minimumDisplaySeverity">Minimum severity level for a message to be displayed in console</param>
         public Logger(string logPath, Severity minimumDisplaySeverity)
         {
             this.logPath = logPath;
             this.MinimumDisplaySeverity = minimumDisplaySeverity;
         }
 
+        /// <summary>
+        /// Appends a message to the console and log file with a timestamp and severity level.
+        /// </summary>
+        /// <param name="severity">Severity level</param>
+        /// <param name="message">Message</param>
         public void Log(Severity severity, string message)
         {
             // Only write to the console if the severity meets the minimum
