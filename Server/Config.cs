69926d7c35824d71eb7bea87eb73105e5684a5d7
Thomotron <gigatech55@gmail.com>
2018-08-20 19:16:05 +0930

    Add documentation

--- Server/Config.cs
+++ Server/Config.cs
@@ -5,6 +5,7 @@ using System.Xml;
 
 namespace PhinixServer
 {
+    /// <inheritdoc />
     /// <summary>
     /// Server configuration class able to read from and write to a configuration file.
     /// </summary>
@@ -14,24 +15,48 @@ namespace PhinixServer
         // This will hold any excess data that doesn't fit in the current version of this class
         public ExtensionDataObject ExtensionData { get; set; }
 
+        /// <summary>
+        /// IP address to listen on.
+        /// </summary>
         [IgnoreDataMember]
         public IPAddress Address = IPAddress.Any;
 
+        /// <summary>
+        /// Textual representation of Address.
+        /// This is reserved for (de)serialisation purposes.
+        /// </summary>
         [DataMember(Name = "IPAddress")]
         private string addressString = "";
 
+        /// <summary>
+        /// Port to listen on.
+        /// </summary>
         [DataMember(Name = "Port")]
         public int Port = 16180;
 
+        /// <summary>
+        /// Path to the log file.
+        /// </summary>
         [DataMember(Name = "LogFile")]
         public string LogPath = "server.log";
 
+        /// <summary>
+        /// The minimum verbosity level for a message to be displayed in the console.
+        /// </summary>
         [DataMember(Name = "DisplayVerbosity")]
         public Verbosity DisplayVerbosity = Verbosity.INFO;
 
+        /// <summary>
+        /// The minimum verbosity level for a message to be recorded in the log file.
+        /// </summary>
         [DataMember(Name = "LogVerbosity")]
         public Verbosity LogVerbosity = Verbosity.INFO;
 
+        /// <summary>
+        /// Loads a <c>Config</c> object from the given file path. Will return a default <c>Config</c> if the file does not exist.
+        /// </summary>
+        /// <param name="filePath">Config file path</param>
+        /// <returns>Loaded <c>Config</c> object</returns>
         public static Config Load(string filePath)
         {
             // Give a fresh new config if the given file doesn't exist
@@ -47,6 +72,11 @@ namespace PhinixServer
             return result;
         }
 
+        /// <summary>
+        /// Saves the <c>Config</c> object to an XML document at the given path.
+        /// This will overwrite the file if it already exists.
+        /// </summary>
+        /// <param name="filePath">Destination file path</param>
         public void Save(string filePath)
         {
             XmlWriterSettings settings = new XmlWriterSettings { Indent = true };
@@ -56,12 +86,22 @@ namespace PhinixServer
             }
         }
         
+        /// <summary>
+        /// Called before the <c>Config</c> is serialised.
+        /// Used to convert complex types to something easier to edit by hand.
+        /// </summary>
+        /// <param name="context"></param>
         [OnSerializing]
         private void OnSerializing(StreamingContext context)
         {
             this.addressString = Address.ToString();
         }
         
+        /// <summary>
+        /// Called after the <c>Config</c> is deserialised.
+        /// Used to convert easy-to-edit types back into their complex counterparts.
+        /// </summary>
+        /// <param name="context"></param>
         [OnDeserialized]
         private void OnDeserialised(StreamingContext context)
         {
