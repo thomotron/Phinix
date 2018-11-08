25c4aac1a3fa8f1b368d00f2caf712e68b2b27ef
Thomas Wade <gigatech55@gmail.com>
2018-11-08 21:54:11 +1030

    Add ILoggable interface
    Adds a common interface for modules to hook into the logging functionality of the implementing app.

---
+++ Common/Utils/LogEventArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Utils
+{
+    /// <summary>
+    /// Event arguments containing a log message and whether it is an error.
+    /// </summary>
+    public class LogEventArgs : EventArgs
+    {
+        /// <summary>
+        /// The log message.
+        /// </summary>
+        public string Message;
+        
+        /// <summary>
+        /// The level of severity for the message.
+        /// </summary>
+        public LogLevel LogLevel;
+        
+        public LogEventArgs(string message, LogLevel logLevel = LogLevel.INFO)
+        {
+            this.Message = message;
+            this.LogLevel = logLevel;
+        }
+    }
+}
---
+++ Common/Utils/LogLevel.cs
@@ -0,0 +1,14 @@
+﻿namespace Utils
+{
+    /// <summary>
+    /// A common severity level enum for use with <c>LogEventArgs</c>.
+    /// </summary>
+    public enum LogLevel
+    {
+        DEBUG,
+        INFO,
+        WARNING,
+        ERROR,
+        FATAL
+    }
+}
