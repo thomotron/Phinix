14d134318e21f67b78f6a56073669dfd66badff5
Thomas Wade <gigatech55@gmail.com>
2018-11-26 20:11:45 +1030

    Move common protobuf message parsing stuff to Utils

---
+++ Common/Utils/ProtobufPacketHelper.cs
@@ -0,0 +1,48 @@
+using System;
+using Google.Protobuf.WellKnownTypes;
+
+namespace Utils
+{
+    public class ProtobufPacketHelper
+    {
+        /// <summary>
+        /// Attempts to validate a serialised packet.
+        /// If this method succeeds it can be assumed safe to process the packet further.
+        /// </summary>
+        /// <param name="_namespace">Namespace the packet is handled in</param>
+        /// <param name="module">Module the packet is handled in</param>
+        /// <param name="targetModule">Target module of the packet</param>
+        /// <param name="data">Serialised packet</param>
+        /// <param name="parsedMessage">Parsed packet as an <c>Any</c> message</param>
+        /// <param name="typeUrl">Parsed TypeUrl</param>
+        /// <returns>Packet was validated successfully</returns>
+        public static bool ValidatePacket(string _namespace, string module, string targetModule, byte[] data, out Any parsedMessage, out TypeUrl typeUrl)
+        {
+            // Initialise out variables
+            parsedMessage = null;
+            typeUrl = null;
+            
+            // Make sure the packet is destined for this module, just in case
+            if (!targetModule.Equals(module)) return false;
+            
+            // Parse the incoming message
+            parsedMessage = Any.Parser.ParseFrom(data);
+            
+            // Get the TypeUrl from the message to help determine what it actually is
+            try
+            {
+                typeUrl = new TypeUrl(parsedMessage.TypeUrl);
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+            
+            // Check that the message's namespace matches the one we will be using with our packets
+            if (!typeUrl.Namespace.Equals(_namespace)) return false;
+            
+            // Nothing bad has happened so far, so the packet is clear for further processing
+            return true;
+        }
+    }
+}
--- Common/Utils/TypeUrl.cs
+++ Common/Utils/TypeUrl.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Text.RegularExpressions;
 
-namespace Connections
+namespace Utils
 {
     public class TypeUrl
     {
