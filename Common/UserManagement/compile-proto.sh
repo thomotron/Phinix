84dfee74c9c2425c2bc309ec8c677a8d4587fbf0
Thomas Wade <gigatech55@gmail.com>
2018-11-29 15:35:00 +1030

    Add LoginPacket protobuf to UserManagement
    Prep for new login process.
    Authentication will be solely handling authentication, login will be a separate process handled by the UserManagement module.

--- Common/UserManagement/compile-proto.sh
+++ Common/UserManagement/compile-proto.sh
@@ -4,4 +4,8 @@
 echo "Compiling users"
 protoc --proto_path=./ --csharp_out=./Users/compiled/ ./Users/*.proto
 
+# Packets
+echo "Compiling packets"
+protoc --proto_path=./ --csharp_out=./Packets/compiled/ ./Packets/*.proto
+
 echo "Done"
