ebd81bb83b6297a01e33883c7f3b045e9cd663ee
Thomas Wade <gigatech55@gmail.com>
2018-12-01 19:16:39 +1030

    Add new properties and change constructor for client user manager

--- Common/UserManagement/ClientUserManager.cs
+++ Common/UserManagement/ClientUserManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Connections;
 using Utils;
 
 namespace UserManagement
@@ -14,6 +15,37 @@ namespace UserManagement
         public override event EventHandler<LogEventArgs> OnLogEntry;
         /// <inheritdoc />
         public override void RaiseLogEntry(LogEventArgs e) => OnLogEntry?.Invoke(this, e);
+
+        /// <summary>
+        /// Whether the client is logged in to the server.
+        /// </summary>
+        public bool LoggedIn { get; private set; }
+        
+        /// <summary>
+        /// UUID used by other users to identify the user.
+        /// </summary>
+        public string Uuid { get; private set; }
+
+        /// <summary>
+        /// <c>NetClient</c> to send packets through and bind events to.
+        /// </summary>
+        private NetClient netClient;
+
+        /// <summary>
+        /// Display name of the user.
+        /// </summary>
+        private string displayName;
+
+        /// <summary>
+        /// Whether the server should update it's copy of the user's display name with the one provided on login.
+        /// </summary>
+        private bool useServerDisplayName;
+
+        /// <summary>
+        /// Session identifier to use when sending packets to the server.
+        /// Set through <c>SendLoginPacket</c>.
+        /// </summary>
+        private string sessionId;
         
         /// <summary>
         /// Stores each user in an easily-serialisable format.
@@ -24,8 +56,12 @@ namespace UserManagement
         /// </summary>
         private object userStoreLock = new object();
 
-        public ClientUserManager()
+        public ClientUserManager(NetClient netClient, string displayName, bool useServerDisplayName = false)
         {
+            this.netClient = netClient;
+            this.displayName = displayName;
+            this.useServerDisplayName = useServerDisplayName;
+            
             this.userStore = new UserStore();
         }
     }
