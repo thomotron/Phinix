53c828ea17e4fe07d643d06d0d2643c31073dd00
Thomas Wade <gigatech55@gmail.com>
2018-12-05 15:04:08 +1030

    Add ClientChat class

---
+++ Common/Chat/ClientChat.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Authentication;
+using Connections;
+using Google.Protobuf;
+using Google.Protobuf.WellKnownTypes;
+using UserManagement;
+using Utils;
+
+namespace Chat
+{
+    class ClientChat : Chat
+    {
+        /// <inheritdoc/>
+        public override event EventHandler<LogEventArgs> OnLogEntry;
+        /// <inheritdoc/>
+        public override void RaiseLogEntry(LogEventArgs e) => OnLogEntry?.Invoke(this, e);
+
+        /// <summary>
+        /// <c>NetClient</c> instance to bind the packet handler to.
+        /// </summary>
+        private NetClient netClient;
+
+        /// <summary>
+        /// <c>ClientAuthenticator</c> to get the session ID from.
+        /// </summary>
+        private ClientAuthenticator authenticator;
+
+        /// <summary>
+        /// <c>ClientUserManager</c> used for user lookup and display name rendering
+        /// </summary>
+        private ClientUserManager userManager;
+        
+        public ClientChat(NetClient netClient, ClientAuthenticator authenticator, ClientUserManager userManager)
+        {
+            this.netClient = netClient;
+            this.authenticator = authenticator;
+            this.userManager = userManager;
+            
+            netClient.RegisterPacketHandler(MODULE_NAME, packetHandler);
+        }
+
+        /// <summary>
+        /// Handles incoming packets.
+        /// </summary>
+        /// <param name="module">Target module</param>
+        /// <param name="connectionId">Original connection ID</param>
+        /// <param name="data">Data payload</param>
+        private void packetHandler(string module, string connectionId, byte[] data)
+        {
+            // Discard packet if it fails validation
+            if (!ProtobufPacketHelper.ValidatePacket("Chat", MODULE_NAME, module, data, out Any message, out TypeUrl typeUrl)) return;
+
+            // Determine what to do with the packet
+            switch (typeUrl.Type)
+            {
+                case "ChatMessagePacket":
+                    RaiseLogEntry(new LogEventArgs("Got a ChatMessagePacket", LogLevel.DEBUG));
+                    break;
+                default:
+                    RaiseLogEntry(new LogEventArgs("Got an unknown packet type (" + typeUrl.Type + "), discarding...", LogLevel.DEBUG));
+                    break;
+            }
+        }
+    }
+}
