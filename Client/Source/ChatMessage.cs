fd617fb250db2b7abd5083ac3bc071541eae0b9a
Thomas Wade <gigatech55@gmail.com>
2018-12-05 21:59:49 +1030

    Add ChatMessage class for UI

---
+++ Client/Source/ChatMessage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PhinixClient
+{
+    public class ChatMessage
+    {
+        /// <summary>
+        /// Time message was received.
+        /// Set when the constructor is run.
+        /// </summary>
+        public DateTime ReceivedTime;
+        
+        /// <summary>
+        /// UUID of the sender.
+        /// </summary>
+        public string SenderUuid;
+        
+        /// <summary>
+        /// The message itself.
+        /// </summary>
+        public string Message;
+        
+        public ChatMessage(string senderUuid, string message)
+        {
+            this.SenderUuid = senderUuid;
+            this.Message = message;
+            
+            this.ReceivedTime = DateTime.UtcNow;
+        }
+
+        public ChatMessage(string senderUuid, string message, DateTime receivedTime)
+        {
+            this.ReceivedTime = receivedTime;
+            this.SenderUuid = senderUuid;
+            this.Message = message;
+        }
+    }
+}
