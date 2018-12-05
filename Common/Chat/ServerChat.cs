ed9c0b8ec355b000bda7fa4dd20715d50fb7ca9c
Thomas Wade <gigatech55@gmail.com>
2018-12-05 15:17:45 +1030

    Add server-side chat class

---
+++ Common/Chat/ServerChat.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Authentication;
+using Connections;
+using Google.Protobuf.WellKnownTypes;
+using UserManagement;
+using Utils;
+
+namespace Chat
+{
+    class ServerChat : Chat
+    {
+        /// <inheritdoc/>
+        public override event EventHandler<LogEventArgs> OnLogEntry;
+        /// <inheritdoc/>
+        public override void RaiseLogEntry(LogEventArgs e) => OnLogEntry?.Invoke(this, e);
+
+        /// <summary>
+        /// Raised when a chat message is received.
+        /// </summary>
+        public event EventHandler<ChatMessageEvent> OnChatMessageReceived;
+        
+        /// <summary>
+        /// <c>NetServer</c> instance to bind the packet handler to.
+        /// </summary>
+        private NetServer netServer;
+
+        /// <summary>
+        /// <c>ServerAuthenticator</c> to get the session ID from.
+        /// </summary>
+        private ServerAuthenticator authenticator;
+
+        /// <summary>
+        /// <c>ServerUserManager</c> used for user lookup and display name rendering
+        /// </summary>
+        private ServerUserManager userManager;
+        
+        public ServerChat(NetServer netServer, ServerAuthenticator authenticator, ServerUserManager userManager)
+        {
+            this.netServer = netServer;
+            this.authenticator = authenticator;
+            this.userManager = userManager;
+            
+            netServer.RegisterPacketHandler(MODULE_NAME, packetHandler);
+        }
+
+        /// <summary>
+        /// Handles incoming packets.
+        /// </summary>
+        /// <param name="module">Target module</param>
+        /// <param name="connectionId">Original connection ID</param>
+        /// <param name="data">Data payload</param>
+        private void packetHandler(string module, string connectionId, byte[] data)
+        {
+            // Discard packet if it fails validation
+            if (!ProtobufPacketHelper.ValidatePacket("Chat", MODULE_NAME, module, data, out Any message, out TypeUrl typeUrl)) return;
+
+            // Determine what to do with the packet
+            switch (typeUrl.Type)
+            {
+                case "ChatMessagePacket":
+                    RaiseLogEntry(new LogEventArgs("Got a ChatMessagePacket", LogLevel.DEBUG));
+                    break;
+                default:
+                    RaiseLogEntry(new LogEventArgs("Got an unknown packet type (" + typeUrl.Type + "), discarding...", LogLevel.DEBUG));
+                    break;
+            }
+        }
+    }
+}
