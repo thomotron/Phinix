0f19d859dcb5e22828ce660c34899a8f27573456
Thomas Wade <gigatech55@gmail.com>
2018-12-18 14:57:12 +1030

    Add comments

--- Common/Utils/TextHelper.cs
+++ Common/Utils/TextHelper.cs
@@ -13,10 +13,17 @@ namespace Utils
 		/// </summary>
 		private static readonly string[] strippableTags = {"size", "b", "i", "color"};
 
+		/// <summary>
+		/// Strips the given set of tags from the input string.
+		/// </summary>
+		/// <param name="input">String to strip</param>
+		/// <param name="strippedTags">Tags to strip from the input string</param>
+		/// <returns>Stripped input</returns>
 		private static string stripRichText(string input, params string[] strippedTags)
 		{
 			foreach (string tag in strippedTags) {
-				// Maybe a better way than a Regex to parse RichText ?
+				// Maybe a better way than a Regex to parse RichText?
+				// Unfortunately not, I'm afraid
 				string pattern = @"<\/?" + tag + @"(=[\w#]+)?>";
 
 				Regex regex = new Regex(pattern);
@@ -26,6 +33,11 @@ namespace Utils
 			return input;
 		}
 
+		/// <summary>
+		/// Returns the given string with all rich text tags removed.
+		/// </summary>
+		/// <param name="input">String to strip</param>
+		/// <returns>Stripped input</returns>
 		public static string StripRichText(string input)
 		{
 			return stripRichText(input, strippableTags);
