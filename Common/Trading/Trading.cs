b2e8d637403f1ca71fd6e28e04afadca11a11649
Thomas Wade <gigatech55@gmail.com>
2018-12-18 18:38:26 +1030

    Add common trading module class

--- Common/Trading/Trading.cs
+++ Common/Trading/Trading.cs
@@ -1,7 +1,18 @@
 ﻿using System;
 using System.Reflection;
+using Utils;
 
 namespace Trading
 {
+    public abstract class Trading : ILoggable
+    {
+        public static readonly Version Version = Assembly.GetAssembly(typeof(Trading)).GetName().Version;
+
+        public const string MODULE_NAME = "trade";
+
+        /// <inheritdoc />
+        public abstract event EventHandler<LogEventArgs> OnLogEntry;
+        /// <inheritdoc />
+        public abstract void RaiseLogEntry(LogEventArgs e);
     }
 }
