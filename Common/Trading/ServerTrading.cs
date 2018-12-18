7302bb30ceb9aec292dc20e28d5aac0337312820
Thomas Wade <gigatech55@gmail.com>
2018-12-18 18:41:25 +1030

    Add basic ServerTrading class with packet handling

---
+++ Common/Trading/ServerTrading.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using Authentication;
+using Connections;
+using Google.Protobuf.WellKnownTypes;
+using UserManagement;
+using Utils;
+
+namespace Trading
+{
+    public class ServerTrading : Trading
+    {
+        /// <inheritdoc />
+        public override event EventHandler<LogEventArgs> OnLogEntry;
+        /// <inheritdoc />
+        public override void RaiseLogEntry(LogEventArgs e) => OnLogEntry?.Invoke(this, e);
+
+        /// <summary>
+        /// <c>NetServer</c> instance to bind events and send data through.
+        /// </summary>
+        private NetServer netServer;
+
+        /// <summary>
+        /// <c>ServerAuthenticator</c> instance to check session validity.
+        /// </summary>
+        private ServerAuthenticator authenticator;
+
+        /// <summary>
+        /// <c>ServerUserManager</c> instance to check login state and trade preferences through.
+        /// </summary>
+        private ServerUserManager userManager;
+
+        public ServerTrading(NetServer netServer, ServerAuthenticator authenticator, ServerUserManager userManager)
+        {
+            this.netServer = netServer;
+            this.authenticator = authenticator;
+            this.userManager = userManager;
+            
+            netServer.RegisterPacketHandler(MODULE_NAME, packetHandler);
+        }
+
+        /// <summary>
+        /// Handles incoming packets from <c>NetCommon</c>.
+        /// </summary>
+        /// <param name="module">Target module</param>
+        /// <param name="connectionId">Original connection ID</param>
+        /// <param name="data">Data payload</param>
+        private void packetHandler(string module, string connectionId, byte[] data)
+        {
+            // Validate the incoming packet and discard it if validation fails
+            if (!ProtobufPacketHelper.ValidatePacket("Trading", MODULE_NAME, module, data, out Any message, out TypeUrl typeUrl)) return;
+
+            // Determine what to do with this packet type
+            switch (typeUrl.Type)
+            {
+                default:
+                    RaiseLogEntry(new LogEventArgs("Got an unknown packet type (" + typeUrl.Type + "), discarding...", LogLevel.DEBUG));
+                    break;
+            }
+        }
+    }
+}
