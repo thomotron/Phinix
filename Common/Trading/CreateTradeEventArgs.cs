57d924b06af358d5fa58e08c47304d58aa55e0d8
Thomas Wade <gigatech55@gmail.com>
2018-12-23 13:11:24 +1030

    Add CreateTrade event args

---
+++ Common/Trading/CreateTradeEventArgs.cs
@@ -0,0 +1,56 @@
+using System;
+
+namespace Trading
+{
+    public class CreateTradeEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Whether the trade creation was successful.
+        /// </summary>
+        public bool Success;
+
+        /// <summary>
+        /// Trade ID
+        /// </summary>
+        public string TradeId;
+
+        /// <summary>
+        /// Other party's UUID.
+        /// </summary>
+        public string OtherPartyUuid;
+
+        /// <summary>
+        /// Reason the trade failed to create.
+        /// </summary>
+        public TradeFailureReason FailureReason;
+
+        /// <summary>
+        /// Message from the server.
+        /// </summary>
+        public string FailureMessage;
+
+        /// <summary>
+        /// Creates a successful <c>CreateTradeResponseArgs</c> with the given trade ID and UUID.
+        /// </summary>
+        /// <param name="tradeId">Trade ID</param>
+        /// <param name="otherPartyUuid">Other party's UUID</param>
+        public CreateTradeEventArgs(string tradeId, string otherPartyUuid)
+        {
+            this.Success = true;
+            this.TradeId = tradeId;
+            this.OtherPartyUuid = otherPartyUuid;
+        }
+
+        /// <summary>
+        /// Creates a failed <c>CreateTradeResponseArgs</c> with the given failure reason and message.
+        /// </summary>
+        /// <param name="failureReason">Reason the trade failed to create</param>
+        /// <param name="failureMessage">Message from the server</param>
+        public CreateTradeEventArgs(TradeFailureReason failureReason, string failureMessage)
+        {
+            this.Success = false;
+            this.FailureReason = failureReason;
+            this.FailureMessage = failureMessage;
+        }
+    }
+}
