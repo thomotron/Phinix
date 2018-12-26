0310eab2452c7aaf72d5cfb129e179a51a64d7cf
Thomas Wade <gigatech55@gmail.com>
2018-12-26 15:23:51 +1030

    Add TradeWindow class

---
+++ Client/Source/TradeWindow.cs
@@ -0,0 +1,12 @@
+using RimWorld;
+using UnityEngine;
+using Verse;
+using static PhinixClient.Client;
+
+namespace PhinixClient
+{
+    public class TradeWindow : Window
+    {
+        public override Vector2 InitialSize => new Vector2(1000f, 750f);
+    }
+}
