2cef5d4490b877e1f0164324d49e06b68a96e282
Thomas Wade <gigatech55@gmail.com>
2018-12-27 17:43:15 +1030

    Add UpdateTradeStatus packet handler

--- Common/Trading/ServerTrading.cs
+++ Common/Trading/ServerTrading.cs
@@ -75,6 +75,10 @@ namespace Trading
                     RaiseLogEntry(new LogEventArgs(string.Format("Got an UpdateTradeItemsPacket from {0}", connectionId), LogLevel.DEBUG));
                     updateTradeItemsPacketHandler(connectionId, message.Unpack<UpdateTradeItemsPacket>());
                     break;
+                case "UpdateTradeStatusPacket":
+                    RaiseLogEntry(new LogEventArgs(string.Format("Got an UpdateTradeStatusPacket from {0}", connectionId), LogLevel.DEBUG));
+                    updateTradeStatusPacketHandler(connectionId, message.Unpack<UpdateTradeStatusPacket>());
+                    break;
                 default:
                     RaiseLogEntry(new LogEventArgs("Got an unknown packet type (" + typeUrl.Type + "), discarding...", LogLevel.DEBUG));
                     break;
@@ -201,6 +205,167 @@ namespace Trading
                 Success = false,
                 FailureReason = failureReason,
                 FailureMessage = failureMessage
+            };
+            Any packedPacket = ProtobufPacketHelper.Pack(packet);
+            
+            // Send it on its way
+            netServer.Send(connectionId, MODULE_NAME, packedPacket.ToByteArray());
+        }
+        
+        /// <summary>
+        /// Handles incoming <c>UpdateTradeStatusPacket</c>s.
+        /// </summary>
+        /// <param name="connectionId">Original connection ID</param>
+        /// <param name="packet">Incoming <c>UpdateTradeStatusPacket</c></param>
+        private void updateTradeStatusPacketHandler(string connectionId, UpdateTradeStatusPacket packet)
+        {
+            // Ignore packets from non-authenticated and non-logged in users
+            if (!authenticator.IsAuthenticated(connectionId, packet.SessionId)) return;
+            if (!userManager.IsLoggedIn(connectionId, packet.Uuid)) return;
+
+            lock (activeTradesLock)
+            {
+                Trade trade = activeTrades[packet.TradeId];
+                
+                // Try to get the other party's UUID, returning on failure
+                if (!trade.TryGetOtherParty(packet.Uuid, out string otherPartyUuid)) return;
+                
+                // Check if the trade is being cancelled
+                if (packet.Cancelled)
+                {
+                    // Try to get the sender's items on offer, returning on failure
+                    if (!trade.TryGetItemsOnOffer(packet.Uuid, out Thing[] items)) return;
+                    
+                    // Return the sender's items to them
+                    sendCompleteTradePacket(connectionId, trade.TradeId, true, otherPartyUuid, items);
+                    
+                    // Try to get the other party's items on offer, returning on failure
+                    if (!trade.TryGetItemsOnOffer(otherPartyUuid, out Thing[] otherPartyItems)) return;
+                    
+                    // Check if the other party is logged in
+                    if (!userManager.TryGetLoggedIn(otherPartyUuid, out bool otherPartyLoggedIn) || !otherPartyLoggedIn) return;
+                    
+                    // Try to get the other party's connection ID
+                    if (!userManager.TryGetConnection(otherPartyUuid, out string otherPartyConnectionId)) return;
+                    
+                    // Return the other party's items to them
+                    sendCompleteTradePacket(otherPartyConnectionId, trade.TradeId, true, packet.Uuid, otherPartyItems);
+                    
+                    // Remove the trade
+                    activeTrades.Remove(packet.TradeId);
+                }
+                else
+                {
+                    // Check if the sender has just accepted
+                    if (packet.Accepted && !trade.AcceptedParties.Contains(packet.Uuid))
+                    {
+                        // Add the sender's UUID to the accepted list
+                        trade.AcceptedParties.Add(packet.Uuid);
+                    }
+                    // Check if the sender has just un-accepted
+                    else if (!packet.Accepted && trade.AcceptedParties.Contains(packet.Uuid))
+                    {
+                        // Remove the sender's UUID from the accepted list
+                        trade.AcceptedParties.Remove(packet.Uuid);
+                    }
+                    else
+                    {
+                        // Nothing has changed, stop here
+                        return;
+                    }
+                    
+                    // Check if all parties have accepted and the trade can be completed successfully
+                    if (trade.AcceptedParties.Count == trade.PartyUuids.Length)
+                    {
+                        // Try to get the sender's items on offer, returning on failure
+                        if (!trade.TryGetItemsOnOffer(packet.Uuid, out Thing[] items)) return;
+                        
+                        // Try to get the other party's items on offer, returning on failure
+                        if (!trade.TryGetItemsOnOffer(otherPartyUuid, out Thing[] otherPartyItems)) return;
+                    
+                        // Give the other party's items to the sender
+                        sendCompleteTradePacket(connectionId, trade.TradeId, false, otherPartyUuid, otherPartyItems);
+                    
+                        // Check if the other party is logged in
+                        if (!userManager.TryGetLoggedIn(otherPartyUuid, out bool otherPartyLoggedIn) || !otherPartyLoggedIn) return;
+                    
+                        // Try to get the other party's connection ID
+                        if (!userManager.TryGetConnection(otherPartyUuid, out string otherPartyConnectionId)) return;
+                    
+                        // Give the sender's items to the other party
+                        sendCompleteTradePacket(otherPartyConnectionId, trade.TradeId, false, packet.Uuid, items);
+                    
+                        // Remove the trade
+                        activeTrades.Remove(packet.TradeId);
+                    }
+                    else
+                    {
+                        // Ok so this next block here might look a little confusing.
+                        // Because these methods are all 'Try...' methods, we need to consider their failure despite that
+                        // only the first one - trying to get the other party's UUID - should fail.
+                        // The other party's UUID and accepted state are necessary to respond to the sender, so that is done
+                        // early.
+                        // The remaining checks are for the other party and are just checking login and getting their
+                        // connection ID.
+                    
+                        // Try to get the other party's accepted state, returning on failure
+                        if (!trade.TryGetAccepted(otherPartyUuid, out bool otherPartyAccepted)) return;
+                    
+                        // Send an update to the sender
+                        sendUpdateTradeStatusPacket(connectionId, trade.TradeId, packet.Accepted, otherPartyAccepted);
+                    
+                        // Check if the other party is logged in
+                        if (!userManager.TryGetLoggedIn(otherPartyUuid, out bool otherPartyLoggedIn) || !otherPartyLoggedIn) return;
+                    
+                        // Try to get the other party's connection ID
+                        if (!userManager.TryGetConnection(otherPartyUuid, out string otherPartyConnectionId)) return;
+                    
+                        // Send an update to the other party
+                        sendUpdateTradeStatusPacket(otherPartyConnectionId, trade.TradeId, otherPartyAccepted, packet.Accepted);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sends a <c>CompleteTradePacket</c> to the given connection ID.
+        /// </summary>
+        /// <param name="connectionId">Destination connection ID</param>
+        /// <param name="tradeId">Trade ID</param>
+        /// <param name="cancelled">Whether the trade was cancelled</param>
+        /// <param name="otherPartyUuid">Other party's UUID</param>
+        /// <param name="items">Collection of items to send</param>
+        private void sendCompleteTradePacket(string connectionId, string tradeId, bool cancelled, string otherPartyUuid, IEnumerable<Thing> items)
+        {
+            // Create and pack a response
+            CompleteTradePacket packet = new CompleteTradePacket
+            {
+                TradeId = tradeId,
+                Success = !cancelled,
+                OtherPartyUuid = otherPartyUuid,
+                Items = {items}
+            };
+            Any packedPacket = ProtobufPacketHelper.Pack(packet);
+                
+            // Send it on its way
+            netServer.Send(connectionId, MODULE_NAME, packedPacket.ToByteArray());
+        }
+        
+        /// <summary>
+        /// Sends an <c>UpdateTradeStatusPacket</c> with the given trade ID and both accepted states to the given connection.
+        /// </summary>
+        /// <param name="connectionId">Destination connection ID</param>
+        /// <param name="tradeId">Trade ID</param>
+        /// <param name="accepted">Whether this party has accepted</param>
+        /// <param name="otherPartyAccepted">Whether the other party has accepted</param>
+        private void sendUpdateTradeStatusPacket(string connectionId, string tradeId, bool accepted, bool otherPartyAccepted)
+        {
+            // Create and pack a response
+            UpdateTradeStatusPacket packet = new UpdateTradeStatusPacket
+            {
+                TradeId = tradeId,
+                Accepted = accepted,
+                OtherPartyAccepted = otherPartyAccepted
             };
             Any packedPacket = ProtobufPacketHelper.Pack(packet);
             
