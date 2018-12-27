84403b612a2b192120377e1020ac08b1c213b8a8
Thomas Wade <gigatech55@gmail.com>
2018-12-27 17:45:17 +1030

    Remove weird-ass using
    wtf how did this happen

--- Common/Trading/ServerTrading.cs
+++ Common/Trading/ServerTrading.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using Authentication;
 using Connections;
 using Google.Protobuf;
