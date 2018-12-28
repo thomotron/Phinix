b0fb56361838a85b0575c940fed3ee9b995ef2c5
Thomas Wade <gigatech55@gmail.com>
2018-12-28 10:55:45 +1030

    Add ItemRow class

---
+++ Client/Source/GUI/ItemRow.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Net.Mime;
+using UnityEngine;
+using Verse;
+
+namespace PhinixClient
+{
+    public class ItemRow : IDrawable
+    {
+        /// <summary>
+        /// The item this row will display.
+        /// </summary>
+        private Thing thing;
+        
+        /// <summary>
+        /// Height of the row.
+        /// </summary>
+        private float height;
+
+        /// <summary>
+        /// Whether to draw an alternate background colour.
+        /// </summary>
+        private bool alternateBackground;
+
+        public ItemRow(Thing thing, float height, bool alternateBackground = false)
+        {
+            this.thing = thing;
+            this.height = height;
+            this.alternateBackground = alternateBackground;
+        }
+        
+        /// <inheritdoc />
+        public void Draw(Rect container)
+        {
+            // Background
+            if (alternateBackground) Widgets.DrawHighlight(container);
+            
+            // Icon
+            Rect iconRect = new Rect(
+                x: container.xMin,
+                y: container.yMin,
+                width: container.height,
+                height: height
+            );
+            Widgets.ThingIcon(iconRect, thing);
+            
+            // Set text alignment
+            TextAnchor oldAnchorPos = Text.Anchor;
+            Text.Anchor = TextAnchor.MiddleLeft;
+            
+            // Item name
+            Rect labelRect = new Rect(
+                x: iconRect.xMax,
+                y: container.yMin,
+                width: Text.CalcSize(thing.Label).x,
+                height: height
+            );
+            Widgets.Label(labelRect, thing.Label);
+            
+            // Item count
+            Rect countRect = new Rect(
+                x: container.xMax - Text.CalcSize(thing.stackCount.ToString()).x,
+                y: container.yMin,
+                width: Text.CalcSize(thing.stackCount.ToString()).x,
+                height: height
+            );
+            Widgets.Label(countRect, thing.stackCount.ToString());
+            
+            // Reset text alignment
+            Text.Anchor = oldAnchorPos;
+        }
+
+        /// <inheritdoc />
+        public float GetHeight(float width)
+        {
+            return height;
+        }
+
+        /// <inheritdoc />
+        /// <exception cref="NotSupportedException"><c>ItemRow</c>s are always drawn at full width</exception>
+        public float GetWidth(float height)
+        {
+            throw new NotSupportedException("ItemRows are always drawn at full width.");
+        }
+    }
+}
