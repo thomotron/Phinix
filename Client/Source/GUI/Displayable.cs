fb96f8eca821f35011926bbb03b070750cd6a3b0
Thomas Wade <gigatech55@gmail.com>
2019-01-03 02:25:42 +1030

    Annotate Displayable class

--- Client/Source/GUI/Displayable.cs
+++ Client/Source/GUI/Displayable.cs
@@ -13,23 +13,45 @@ namespace PhiClient.UI
     {
         public const float FLUID = -1;
 
+        /// <summary>
+        /// Draws the object within the given container.
+        /// </summary>
+        /// <param name="inRect">Container to draw within</param>
         public abstract void Draw(Rect inRect);
 
+        /// <summary>
+        /// Returns whether the object will fill any horizontal space it's given.
+        /// </summary>
+        /// <returns>Whether the object will fill any horizontal space it's given</returns>
         public virtual bool IsFluidWidth()
         {
             return true;
         }
 
+        /// <summary>
+        /// Returns whether the object will fill any vertical space it's given.
+        /// </summary>
+        /// <returns>Whether the object will fill any vertical space it's given</returns>
         public virtual bool IsFluidHeight()
         {
             return true;
         }
 
+        /// <summary>
+        /// Calculates the object's width when constrained within the given height.
+        /// </summary>
+        /// <param name="height">Height to constrain within</param>
+        /// <returns>Object's width</returns>
         public virtual float CalcWidth(float height)
         {
             return Displayable.FLUID;
         }
 
+        /// <summary>
+        /// Calculates the object's height when constrained within the given width.
+        /// </summary>
+        /// <param name="width">Width to constrain within</param>
+        /// <returns>Object's height</returns>
         public virtual float CalcHeight(float width)
         {
             return Displayable.FLUID;
