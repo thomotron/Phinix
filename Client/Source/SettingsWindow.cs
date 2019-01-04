7d87d8b9317ad0168e2e3fb8d526a68f80fd2508
Thomas Wade <gigatech55@gmail.com>
2019-01-04 22:26:23 +1030

    Replace old GUI code with new widgets

--- Client/Source/SettingsWindow.cs
+++ Client/Source/SettingsWindow.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text.RegularExpressions;
 using System.Threading;
+using PhinixClient.GUI;
 using UnityEngine;
 using Verse;
 
@@ -8,6 +9,8 @@ namespace PhinixClient
     class SettingsWindow : Window
     {
         private const float DEFAULT_SPACING = 10f;
+        
+        private const float ROW_HEIGHT = 30f;
 
         private const float SERVER_ADDRESS_LABEL_HEIGHT = 120f;
         private const float SERVER_ADDRESS_LABEL_WIDTH = 60f;
@@ -26,7 +29,7 @@ namespace PhinixClient
         private const float USERNAME_BOX_HEIGHT = 30f;
 
         private const float USERNAME_SET_BUTTON_HEIGHT = 30f;
-        private const float USERNAME_SET_BUTTON_WIDTH = 120f;
+        private const float DISPLAY_NAME_SET_BUTTON_WIDTH = 120f;
 
         public override Vector2 InitialSize => new Vector2(600f, 120f);
 
@@ -40,145 +43,180 @@ namespace PhinixClient
             doCloseX = true;
             doCloseButton = false;
             doWindowBackground = true;
+            
+            // Create a flex container to hold our settings
+            VerticalFlexContainer flexContainer = new VerticalFlexContainer(DEFAULT_SPACING);
+
+            // Keep track of how much height we should allocate to the flex container
+            // This is just to keep all the fluid height elements in check
+            float height = 0f;
 
             // Server details (address and [dis]connect button) container
-            Rect serverDetailRect = new Rect(
-                x: inRect.xMin,
-                y: inRect.yMin + 5f, // 5f offset to avoid overlapping close button
-                width: inRect.width,
-                height: SERVER_ADDRESS_BOX_HEIGHT
-            );
             if (Client.Instance.Connected)
             {
-                DrawConnectedServerDetails(serverDetailRect);
+                flexContainer.Add(GenerateConnectedServerDetails());
             }
             else
             {
-                DrawDisconnectedServerDetails(serverDetailRect);
+                flexContainer.Add(GenerateDisconnectedServerDetails());
             }
+            height += ROW_HEIGHT;
             
             // Display name container
-            Rect displayNameRect = new Rect(
-                x: inRect.xMin,
-                y: serverDetailRect.yMax + DEFAULT_SPACING,
-                width: inRect.width,
-                height: USERNAME_BOX_HEIGHT
-            );
-            if (Client.Instance.Online) DrawDisplayName(displayNameRect);
+            if (Client.Instance.Online)
+            {
+                flexContainer.Add(GenerateEditableDisplayName());
+                height += ROW_HEIGHT + DEFAULT_SPACING;
+            };
+            
+            // Draw the flex container with 5f padding at the top to avoid clipping with the close button and only the
+            // height we've accumulated from the top to avoid having super large widgets
+            flexContainer.Draw(inRect.BottomPartPixels(inRect.height - 5f).TopPartPixels(height));
         }
 
         /// <summary>
-        /// Draws a non-editable server address and disconnect button within a given <c>Rect</c>.
+        /// Generates a non-editable server address and disconnect button.
         /// </summary>
-        /// <param name="container">Container to draw within</param>
-        private void DrawConnectedServerDetails(Rect container)
+        /// <returns><c>HorizontalFlexContainer</c> containing connected server details</returns>
+        private HorizontalFlexContainer GenerateConnectedServerDetails()
         {
+            // Create a flex container as our 'row' to store elements in
+            HorizontalFlexContainer row = new HorizontalFlexContainer();
+            
             // Server address label
-            Rect serverAddressLabelRect = new Rect(
-                x: container.xMin,
-                y: container.yMin,
-                width: container.width - (CONNECT_BUTTON_WIDTH + DEFAULT_SPACING),
-                height: SERVER_ADDRESS_BOX_HEIGHT
+            row.Add(
+                new TextWidget(
+                    text: "Phinix_settings_connectedToLabel".Translate(serverAddress),
+                    anchor: TextAnchor.MiddleLeft
+                )
             );
-            Widgets.Label(serverAddressLabelRect, "Phinix_settings_connectedToLabel".Translate(serverAddress));
 
             // Disconnect button
-            Rect disconnectButtonRect = new Rect(
-                x: container.xMax - CONNECT_BUTTON_WIDTH,
-                y: container.yMin,
-                width: CONNECT_BUTTON_WIDTH,
-                height: CONNECT_BUTTON_HEIGHT
+            row.Add(
+                new Container(
+                    new ButtonWidget(
+                        label: "Phinix_settings_disconnectButton".Translate(),
+                        clickAction: () => Client.Instance.Disconnect()
+                    ),
+                    width: CONNECT_BUTTON_WIDTH
+                )
             );
-            if (Widgets.ButtonText(disconnectButtonRect, "Phinix_settings_disconnectButton".Translate()))
-            {
-                Client.Instance.Disconnect();
-            }
+
+            // Return the generated row
+            return row;
         }
 
         /// <summary>
-        /// Draw an editable server address, editable server port, and connect button within a given <c>Rect</c>.
+        /// Generates an editable server address, editable server port, and connect button.
         /// </summary>
-        /// <param name="container">Container to draw within</param>
-        private void DrawDisconnectedServerDetails(Rect container)
+        /// <returns><c>HorizontalFlexContainer</c> containing an editable server address, editable server port, and connect button</returns>
+        private HorizontalFlexContainer GenerateDisconnectedServerDetails()
         {
+            // Create a flex container as our 'row' to store elements in
+            HorizontalFlexContainer row = new HorizontalFlexContainer();
+            
             // Address label
-            Rect serverAddressLabelRect = new Rect(
-                x: container.xMin,
-                y: container.yMin,
-                width: SERVER_ADDRESS_LABEL_WIDTH,
-                height: SERVER_ADDRESS_LABEL_HEIGHT
+            row.Add(
+                new Container(
+                    new TextWidget(
+                        text: "Phinix_settings_addressLabel".Translate(),
+                        anchor: TextAnchor.MiddleLeft
+                    ),
+                    width: SERVER_ADDRESS_LABEL_WIDTH
+                )
             );
-            Widgets.Label(serverAddressLabelRect, "Phinix_settings_addressLabel".Translate());
 
             // Server address box
-            Rect serverAddressBoxRect = new Rect(
-                x: container.xMin + (SERVER_ADDRESS_LABEL_WIDTH + DEFAULT_SPACING),
-                y: container.yMin,
-                width: container.width - (CONNECT_BUTTON_WIDTH + SERVER_PORT_BOX_WIDTH + SERVER_PORT_LABEL_WIDTH + SERVER_ADDRESS_LABEL_WIDTH + DEFAULT_SPACING * 4),
-                height: SERVER_ADDRESS_BOX_HEIGHT
+            row.Add(
+                new TextFieldWidget(
+                    text: serverAddress,
+                    onChange: newAddress => serverAddress = newAddress 
+                )
             );
-            serverAddress = Widgets.TextField(serverAddressBoxRect, serverAddress);
 
             // Port label
-            Rect serverPortLabelRect = new Rect(
-                x: container.xMax - (CONNECT_BUTTON_WIDTH + SERVER_PORT_BOX_WIDTH + SERVER_PORT_LABEL_WIDTH + DEFAULT_SPACING * 2),
-                y: container.yMin,
-                width: SERVER_PORT_LABEL_WIDTH,
-                height: SERVER_PORT_LABEL_HEIGHT
+            row.Add(
+                new Container(
+                    new TextWidget(
+                        text: "Phinix_settings_portLabel".Translate(),
+                        anchor: TextAnchor.MiddleLeft
+                    ),
+                    width: SERVER_PORT_LABEL_WIDTH
+                )
             );
-            Widgets.Label(serverPortLabelRect, "Phinix_settings_portLabel".Translate());
 
             // Server port box
-            Rect serverPortBoxRect = new Rect(
-                x: container.xMax - (CONNECT_BUTTON_WIDTH + SERVER_PORT_BOX_WIDTH + DEFAULT_SPACING),
-                y: container.yMin, 
-                width: SERVER_PORT_BOX_WIDTH,
-                height: SERVER_PORT_BOX_HEIGHT
+            row.Add(
+                new Container(
+                    new TextFieldWidget(
+                        text: serverPortString,
+                        onChange: newPortString =>
+                        {
+                            if (new Regex("(^[0-9]{0,5}$)").IsMatch(newPortString))
+                            {
+                                serverPortString = newPortString;
+                            }
+                        }
+                    ),
+                    width: SERVER_PORT_BOX_WIDTH
+                )
             );
-            serverPortString = Widgets.TextField(serverPortBoxRect, serverPortString, 5, new Regex("(^[0-9]{0,5}$)")); // Regex matches up to 5 digits or nothing at all
 
             // Connect button
-            Rect connectButtonRect = new Rect(
-                x: container.xMax - CONNECT_BUTTON_WIDTH,
-                y: container.yMin,
-                width: CONNECT_BUTTON_WIDTH,
-                height: CONNECT_BUTTON_HEIGHT
-            );
-            if (Widgets.ButtonText(connectButtonRect, "Phinix_settings_connectButton".Translate()))
-            {
-                // Save the connection details to the client settings
-                Client.Instance.ServerAddress = serverAddress;
-                Client.Instance.ServerPort = int.Parse(serverPortString);
+            row.Add(
+                new Container(
+                    new ButtonWidget(
+                        label: "Phinix_settings_connectButton".Translate(),
+                        clickAction: () =>
+                        {
+                            // Save the connection details to the client settings
+                            Client.Instance.ServerAddress = serverAddress;
+                            Client.Instance.ServerPort = int.Parse(serverPortString);
 
-                // Run this on another thread otherwise the UI will lock up.
-                new Thread(() => {
-                    Client.Instance.Connect(serverAddress, int.Parse(serverPortString)); // Assume the port was safely validated by the regex
-                }).Start();
-            }
+                            // Run this on another thread otherwise the UI will lock up.
+                            new Thread(() => {
+                                Client.Instance.Connect(serverAddress, int.Parse(serverPortString)); // Assume the port was safely validated by the regex
+                            }).Start();
+                        }
+                    ),
+                    width: CONNECT_BUTTON_WIDTH
+                )
+            );
+            
+            // Return the generated row
+            return row;
         }
         
-        private void DrawDisplayName(Rect container)
+        /// <summary>
+        /// Generates an editable display name field and a button to apply the changes.
+        /// </summary>
+        /// <returns><c>HorizontalFlexContainer</c> containing an editable display name field and a button to apply the changes</returns>
+        private HorizontalFlexContainer GenerateEditableDisplayName()
         {
+            // Create a flex container as our 'row' to store elements in
+            HorizontalFlexContainer row = new HorizontalFlexContainer();
+            
             // Editable display name text box
-            Rect displayNameRect = new Rect(
-                x: container.xMin,
-                y: container.yMin,
-                width: container.width - (USERNAME_SET_BUTTON_WIDTH + DEFAULT_SPACING),
-                height: USERNAME_BOX_HEIGHT
+            row.Add(
+                new TextFieldWidget(
+                    text: displayName,
+                    onChange: newDisplayName => displayName = newDisplayName
+                )
             );
-            displayName = Widgets.TextField(displayNameRect, displayName);
             
             // Set display name button
-            Rect setDisplayNameButtonRect = new Rect(
-                x: container.xMax - USERNAME_SET_BUTTON_WIDTH,
-                y: container.yMin,
-                width: USERNAME_SET_BUTTON_WIDTH,
-                height: USERNAME_SET_BUTTON_HEIGHT
+            row.Add(
+                new Container(
+                    new ButtonWidget(
+                        label: "Phinix_settings_setDisplayNameButton".Translate(),
+                        clickAction: () => Client.Instance.UpdateDisplayName(displayName)
+                    ),
+                    width: DISPLAY_NAME_SET_BUTTON_WIDTH
+                )
             );
-            if (Widgets.ButtonText(setDisplayNameButtonRect, "Phinix_settings_setDisplayNameButton".Translate()))
-            {
-                Client.Instance.UpdateDisplayName(displayName);
-            }
+            
+            // Return the generated row
+            return row;
         }
     }
 }
