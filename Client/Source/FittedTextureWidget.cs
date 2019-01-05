38c3e85e50f2b4741bc21c59cb85f1026ddedc6d
Thomas Wade <gigatech55@gmail.com>
2019-01-05 13:48:37 +1030

    Add FittedTexture widget

---
+++ Client/Source/FittedTextureWidget.cs
@@ -0,0 +1,31 @@
+using PhinixClient.GUI;
+using UnityEngine;
+using Verse;
+
+namespace PhinixClient
+{
+    public class FittedTextureWidget : Displayable
+    {
+        /// <summary>
+        /// The texture.
+        /// </summary>
+        private Texture2D texture;
+
+        /// <summary>
+        /// Scale of the texture.
+        /// </summary>
+        private float scale;
+
+        public FittedTextureWidget(Texture2D texture, float scale = 1f)
+        {
+            this.texture = texture;
+            this.scale = scale;
+        }
+        
+        /// <inheritdoc />
+        public override void Draw(Rect inRect)
+        {
+            Widgets.DrawTextureFitted(inRect, texture, scale);
+        }
+    }
+}
