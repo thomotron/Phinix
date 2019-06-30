b8f4ae49821771e381a1be6855ad62773ddabcf2
Thomas Wade <gigatech55@gmail.com>
2019-06-30 13:48:10 +0930

    Clean up NetCommon

--- Common/Connections/NetCommon.cs
+++ Common/Connections/NetCommon.cs
@@ -19,8 +19,8 @@ namespace Connections
         internal EventBasedNetListener listener = new EventBasedNetListener();
 
         /// <summary>
-        /// Packet handler callback delegate. Used when registering packet handlers as the callback method.
-        /// Exposes basic information about the incoming packet.
+        /// Packet handler callback delegate. Used as a callback when registering packet handlers.
+        /// Exposes basic information about the incoming packet and a copy of the data.
         /// </summary>
         /// <param name="module">Target module</param>
         /// <param name="connectionId">Original connection ID</param>
@@ -70,12 +70,7 @@ namespace Connections
         /// </summary>
         public void UnregisterAllPacketHandlers()
         {
-            // Get a local snapshot to avoid issues when updating the collection
-            string[] registeredPacketTypes = registeredPacketHandlers.Keys.ToArray();
-            foreach (string packetType in registeredPacketTypes)
-            {
-                UnregisterPacketHandler(packetType);
-            }
+            registeredPacketHandlers.Clear();
         }
         
         /// <summary>
