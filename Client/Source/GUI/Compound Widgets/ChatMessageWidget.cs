acb0df97d06a919b74e200efdd5e14085c437a54
Thomas Wade <gigatech55@gmail.com>
2020-03-15 13:49:59 +1030

    Implement update method for chat messages

--- Client/Source/GUI/Compound Widgets/ChatMessageWidget.cs
+++ Client/Source/GUI/Compound Widgets/ChatMessageWidget.cs
@@ -13,7 +13,6 @@ namespace PhinixClient.GUI
 
         /// <summary>
         /// Time message was received.
-        /// Set when the constructor is run.
         /// </summary>
         public DateTime ReceivedTime;
         
@@ -31,7 +30,12 @@ namespace PhinixClient.GUI
         /// The status of the chat message.
         /// </summary>
         public ChatMessageStatus Status;
-        
+
+        /// <summary>
+        /// The formatted message.
+        /// </summary>
+        private string formattedMessage;
+
         public ChatMessageWidget(string senderUuid, string message)
         {
             this.SenderUuid = senderUuid;
@@ -49,24 +53,6 @@ namespace PhinixClient.GUI
             this.Status = status;
         }
 
-        public string Format()
-        {
-            // Get a local copy of the message
-            string message = Message;
-            
-            // Try to get the display name of the sender
-            if (!Client.Instance.TryGetDisplayName(SenderUuid, out string displayName)) displayName = "???";
-
-            // Strip name formatting if the user wishes not to see it
-            if (!Client.Instance.ShowNameFormatting) displayName = TextHelper.StripRichText(displayName);
-            
-            // Strip message formatting if the user wishes not to see it
-            if (!Client.Instance.ShowChatFormatting) message = TextHelper.StripRichText(message);
-            
-            // Return the formatted message
-            return string.Format("[{0:HH:mm}] {1}: {2}", ReceivedTime.ToLocalTime(), displayName, message);
-        }
-
         /// <inheritdoc />
         public override void Draw(Rect container)
         {
@@ -79,36 +65,61 @@ namespace PhinixClient.GUI
 //                drawContextMenu();
 //            }
 
-            // Get the formatted chat message
-            string formattedText = Format();
+            // Format the message if we haven't already done so
+            if (formattedMessage == null) formattedMessage = format();
             
             // Change the colour of the message to reflect the sent status
             switch (Status)
             {
                 case ChatMessageStatus.PENDING:
-                    formattedText = string.Format("<color=#ffffff80>{0}</color>", TextHelper.StripRichText(formattedText));
+                    formattedMessage = string.Format("<color=#ffffff80>{0}</color>", TextHelper.StripRichText(formattedMessage));
                     break;
                 case ChatMessageStatus.DENIED:
-                    formattedText = string.Format("<color=#f04747>{0}</color>", TextHelper.StripRichText(formattedText));
+                    formattedMessage = string.Format("<color=#f04747>{0}</color>", TextHelper.StripRichText(formattedMessage));
                     break;
                 default:
                     break;
             }
             
-            Widgets.Label(container, formattedText);
+            Widgets.Label(container, formattedMessage);
+        }
+
+        /// <inheritdoc />
+        public override void Update()
+        {
+            // Reformat the message with the latest data
+            formattedMessage = format();
         }
 
         /// <inheritdoc />
         public override float CalcHeight(float width)
         {
             // Return the calculated the height of the formatted text
-            return Text.CalcHeight(Format(), width);
+            return Text.CalcHeight(formattedMessage, width);
         }
 
         /// <inheritdoc />
         public override float CalcWidth(float height)
         {
             return FLUID;
+        }
+
+        private string format()
+        {
+            // Get a local copy of the message
+            string message = Message;
+
+            // Try to get the display name of the sender
+            if (!Client.Instance.TryGetDisplayName(SenderUuid, out string displayName)) displayName = "???";
+
+            // Strip name formatting if the user wishes not to see it
+            if (!Client.Instance.ShowNameFormatting) displayName = TextHelper.StripRichText(displayName);
+
+            // Strip message formatting if the user wishes not to see it
+            if (!Client.Instance.ShowChatFormatting) message = TextHelper.StripRichText(message);
+
+            // Return the formatted message
+            return string.Format("[{0:HH:mm}] {1}: {2}", ReceivedTime.ToLocalTime(), displayName, message);
         }
 
         private void drawContextMenu()
