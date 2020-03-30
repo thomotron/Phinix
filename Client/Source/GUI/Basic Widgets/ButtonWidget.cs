eb94deea6e03a9e67128dba972a73a8d3b92845e
Thomas Wade <gigatech55@gmail.com>
2020-03-30 14:22:29 +1030

    Handle text wrapping in backgroundless buttons
    Fixes #45
    The ButtonText function disables text wrapping for backgroundless buttons which causes issues with long names in the user list, so we'll just do what it does already but with less fat around th edges.

--- Client/Source/GUI/Basic Widgets/ButtonWidget.cs
+++ Client/Source/GUI/Basic Widgets/ButtonWidget.cs
@@ -37,9 +37,24 @@ namespace PhinixClient.GUI
         /// <inheritdoc />
         public override void Draw(Rect inRect)
         {
-            if (Widgets.ButtonText(inRect, label, drawBackground))
+            // Handle wrapping properly since ButtonText won't (as per #45)
+            if (!drawBackground)
             {
-                clickAction();
+                // Draw the text
+                Widgets.Label(inRect, Mouse.IsOver(inRect) ? label.Colorize(Widgets.MouseoverOptionColor) : label);
+
+                // Overlay a button
+                if (Widgets.ButtonInvisible(inRect))
+                {
+                    clickAction();
+                }
+            }
+            else
+            {
+                if (Widgets.ButtonText(inRect, label, drawBackground))
+                {
+                    clickAction();
+                }
             }
         }
 
